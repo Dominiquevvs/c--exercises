4148e02e97c76abb780238373b2448bf60d142bc
Dominiquevvs <dominiquemozaffari@hotmail.com>
2026-03-31 15:53:42 +0200

    simple flow

--- Program.cs
+++ Program.cs
@@ -1,50 +1,32 @@
-﻿using System;
+﻿/*
+// 1. Ask the user for the instrument
+Console.WriteLine("Please enter the name of a music instrument:");
 
-Console.WriteLine("Hello, World!");
-// This code says Hello, World!
+// 2. Input variable
+string instrument = Console.ReadLine();
 
-Random randomizer = new Random();
-// Makes a random number generator 
-bool success = false;
-// boolean to keep track 
-while (success == false)
-// Loop continues until user dices a 6 
-{
-    Console.WriteLine("Roll the dice");
-    // The user presses enter to roll the dice
-    Console.ReadLine();
-    // Create a random number between 1 and 6
-    int dice = randomizer.Next(1, 7); 
-    
-    // numbers 1–6
+// 3.This
+Console.WriteLine($"Can you play the {instrument}?");
 
-    Console.WriteLine("You rolled: " + dice);
-    // The user must roll a 6 to win hejdå
-    if (dice == 6)
-    {
-        Console.WriteLine("Congratulations! You rolled a 6!");
-        success = true;
-    }
-}
+Console.WriteLine("Please enter your familyname:");
+string familyName = Console.ReadLine();
+Console.WriteLine("Please enter your first name:");
+string firstName = Console.ReadLine();
+Console.WriteLine($"{firstName} {familyName} Joined the firm!");
+*/
 
-// Lista med ord som spelet kan välja från
-string[] words = { "cookie", "pie", "cake" };
+// 1. Ask for the full name
+Console.WriteLine("Please enter your first and family name:");
 
-// Skapar en slumpgenerator
-Random random = new Random();
+// 2. Accept the response
+string fullName = Console.ReadLine();
 
-// Väljer ett slumpmässigt ord från listan
-string word = words[random.Next(words.Length)];
+// 3. it splits into two parts
+string[] nameParts = fullName.Split(" ");
 
-// Skapar en array som ska visa _ _ _ istället för bokstäver
-char[] display = new char[word.Length];
+// 4. Transform the cases 
+string firstNameLower = nameParts[0].ToLower();
+string lastNameUpper = nameParts[1].ToUpper();
 
-// Loop som fyller arrayen med _
-for (int i = 0; i < display.Length; i++)
-{
-    display[i] = '_'; // varje plats blir _
-}
-
-// Antal försök spelaren har
-int guesses = 10;
-bool gameOver = false;
+// 5. Write to Console in the format: "LASTNAME, firstname : present"
+Console.WriteLine($"{lastNameUpper}, {firstNameLower} : present");
